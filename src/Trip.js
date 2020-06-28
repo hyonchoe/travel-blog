eb2ed99f384a09d4718fff57b515e6782fd84e58
hyon choe <choe132@hotmail.com>
2020-06-27 22:51:38 -0500

    added in confirmation popups for edit and delete actions

--- src/Trip.js
+++ src/Trip.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Typography, Card } from 'antd'
-import { GlobalOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
-
-
+import { Typography, Card, Modal, Popconfirm } from 'antd'
+import { GlobalOutlined, EditOutlined, DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import './Trip.css'
 
 class Trip extends React.Component {
@@ -16,7 +14,32 @@ class Trip extends React.Component {
      }
 
     onTabChange = () => {
+        //TODO
+    }
+    onGlobeClicked = (curTrip) => {
+        if (curTrip.locations){
+            this.props.launchMapModal(curTrip.title, curTrip.locations)
+        }
+    }
 
+    showDeleteConfirm = (tripId, tripInstance) => {
+        confirm({
+          title: 'Are you sure you want to delete this trip?',
+          icon: <ExclamationCircleOutlined />,
+          content: 'After deletion, it will not be recoverable.',
+          okText: 'Yes',
+          okType: 'danger',
+          cancelText: 'No',
+          onOk() {
+            tripInstance.props.deleteTrip(tripId)
+          },
+          onCancel() {}
+        });
+    }
+    popoverConfirm = (tripId) => {
+        this.props.editTrip(tripId)
+    }
+    popoverCancel = () => {
     }
 
      render() {
@@ -38,6 +61,7 @@ class Trip extends React.Component {
                 tab: 'Pictures',
             },
         ]
+        
         return (
             <div className="tripContainer">
                 <Card
@@ -46,9 +70,16 @@ class Trip extends React.Component {
                     bordered={true}
                     extra={dateStr}
                     actions={[
-                        <GlobalOutlined key="map" onClick={() => this.props.launchMapModal(curTrip.title, curTrip.locations)} />,
-                        <EditOutlined key="edit" onClick={() => this.props.editTrip(curTrip._id) } />,
-                        <DeleteOutlined key="delete" onClick={() => this.props.deleteTrip(curTrip._id) } />,
+                        <GlobalOutlined key="map" onClick={() => this.onGlobeClicked(curTrip) } />,
+                        <Popconfirm
+                            title="Are you sure about editing this trip?"
+                            onConfirm={() => this.popoverConfirm(curTrip._id)}
+                            onCancel={() => this.popoverCancel()}
+                            okText="Yes"
+                            cancelText="No" >
+                            <EditOutlined key="edit" />
+                        </Popconfirm>,
+                        <DeleteOutlined key="delete" onClick={() => this.showDeleteConfirm(curTrip._id, this)} />,
                     ]}
                     tabList={tabList}
                     activeTabKey={ this.state.key }
@@ -59,7 +90,7 @@ class Trip extends React.Component {
                     <div className="divLocation"> {locAddr} </div>
                     }
                     <TripDetails details={curTrip.details} />
-                </Card>
+                </Card>                
             </div>
         )
     }
@@ -86,5 +117,7 @@ const LocationSpans = (locations) => {
         <span className="spanLocation">{loc.fmtAddr}</span>
     ))
 }
+const { confirm } = Modal
+
 
 export default Trip
