1926bb1f62ddf18e77f7774b0a427473687d04ca
hyon choe <choe132@hotmail.com>
2020-07-16 00:07:45 -0500

    added snapshot tests for PrivateRoute

---
+++ src/components/PrivateRoute/index.snapshot.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import PrivateRoute from './'
+
+describe('PrivateRoute', () => {
+    const dummyComponent = (<div className='dummy'></div>)
+    
+    
+    it('matches snapshot for my trips', () => {
+        const handleEditTrip = jest.fn()
+        const wrapper = shallow(
+            <PrivateRoute 
+                path='/myTrips'
+                component={dummyComponent}
+                showMyTrips={true}
+                editTrip={handleEditTrip} />
+        )
+        expect(wrapper).toMatchSnapshot()
+    })
+
+    it('matches snapshot for add trips', () => {
+        const clearEditTrip = jest.fn()
+        const wrapper = shallow(
+            <PrivateRoute
+                path='/addTrip'
+                component={dummyComponent}
+                editTrip={true}
+                clearEditTrip={clearEditTrip} />
+        )
+        expect(wrapper).toMatchSnapshot()
+    })
+})
