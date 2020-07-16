0bb02cb8cdb5331b462cf5358634cc54b4126dc3
hyon choe <choe132@hotmail.com>
2020-07-15 23:44:08 -0500

    added snapshot testing for PictureCarousel

--- src/components/PictureCarousel/index.js
+++ src/components/PictureCarousel/index.js
@@ -3,24 +3,29 @@ import { Carousel } from 'antd'
 import './style.css'
 
 const PictureCarousel = (props) => {
-    const divCarouselImages = (props.images) ? 
-        props.images.map((imgInfo, index) => {
-            return ( 
-                <div key={index}>
-                    <img 
-                        src={imgInfo.S3Url}
-                        alt={imgInfo.name}
-                        className="carouselPicture" />
-                </div>
-            )
-        })
-        : null
+    const divCarouselImages = getCarouselImgs(props.images)
 
     return (
         <Carousel draggable={true}>
             {divCarouselImages}
         </Carousel>
     )
+}
+
+const getCarouselImgs = (images) => {
+    if (!images) return null
+
+    const divs = images.map((imgInfo, index) => {
+        return ( 
+            <div key={index}>
+                <img 
+                    src={imgInfo.S3Url}
+                    alt={imgInfo.name}
+                    className="carouselPicture" />
+            </div>
+        )
+    })
+    return divs
 }
 
 export default PictureCarousel
---
+++ src/components/PictureCarousel/index.snapshot.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import PictureCarousel from './'
+
+describe('PictureCarousel', () => {
+    it('matches snapshot for image divs it hosts', () => {
+        const tripImages = [
+            { name: 'dummyname', S3Url: 'dummyurl' },
+            { name: 'dummyname2', S3Url: 'dummyurl2' },
+        ]
+        const wrapper = shallow(
+            <PictureCarousel images={tripImages} />
+        )
+        expect(wrapper).toMatchSnapshot()
+    })
+})
