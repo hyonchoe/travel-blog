ed19120644f932c059571054e91f2e9c5bb32337
hyon choe <choe132@hotmail.com>
2020-07-17 13:47:38 -0500

    refactored trip data actions from DisplayList to useTripData

---
+++ src/components/DisplayTrips/helpers/useTripData.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import tripService from '../../../services/api'
+
+const useTripData = (showMyTrips) => {
+  const displayLimit = 10
+
+  const { getAccessTokenSilently, isAuthenticated, user } = useAuth0()  
+  
+  const [tripList, setTripList] = useState({
+        trips: [],
+        loadingData: true,
+        noMoreRecords: false,
+      })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = (showMyTrips) ?
+              await tripService.getTrips(getAccessTokenSilently)
+              : await tripService.getPublicTrips(null)
+    
+            setTripList( {
+              ...tripList,
+              trips: res,
+              loadingData: false,
+            })
+          }
+    
+          fetchData()
+    }, [])
+
+    const reloadTripData = async () => {
+        setTripList( {
+          ...tripList,
+          loadingData: true,
+        })
+  
+        const res = await tripService.getPublicTrips(null)
+  
+        setTripList( {
+          trips: res,
+          loadingData: false,
+          noMoreRecords: false,
+        })
+    }
+    const handleDeleteTrip = async (tripId, tripTitle) => {
+      const res = await tripService.deleteTrip(tripId, tripTitle, getAccessTokenSilently)
+      
+      if (res){
+        setTripList({
+          ...tripList,
+          trips: tripList.trips.filter((trip) => { return trip._id !== tripId })
+        })
+      }
+    }
+    const onLoadMore = async () => {
+        setTripList({
+          ...tripList,
+          loadingData: true,
+        })
+        
+        const lastLoadedTrip = tripList.trips[tripList.trips.length-1]
+        const res = await tripService.getPublicTrips(lastLoadedTrip)
+  
+        let updatedData = tripList.trips.concat(res)
+        let scrollToTripId = -1
+        if (updatedData.length > displayLimit){
+          scrollToTripId = lastLoadedTrip._id
+          updatedData = updatedData.slice(updatedData.length - displayLimit)
+          updatedData[0] = { placeholder: true }
+        }
+        const noMoreRecords = res.length === 0 || res[res.length-1].noMoreRecords
+        
+        setTripList({
+          trips: updatedData,
+          loadingData: false,
+          noMoreRecords: noMoreRecords,
+        })
+
+        return {
+          noMoreRecords,
+          scrollToTripId
+        }
+    }
+
+    return { tripList, 
+             reloadTripData, handleDeleteTrip, onLoadMore }
+}
+
+export default useTripData
--- src/components/DisplayTrips/index.js
+++ src/components/DisplayTrips/index.js
@@ -1,100 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Empty, Skeleton, Row, Col, BackTop, message, Modal, Button, List, Typography } from 'antd'
+import React, { useState, useRef } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import tripService from '../../services/api'
+import { Empty, Skeleton, Row, Col, BackTop, message, Modal, Button, List, Typography } from 'antd'
 import MyMapContainer from '../MyMapContainer'
 import Trip from '../Trip'
+import useTripData from './helpers/useTripData'
 
-const DisplayTrips = (props) => {
-    const [tripList, setTripList] = useState({
-      trips: [],
-      loadingData: true,
-      noMoreRecords: false,
-    })
-    
+const DisplayTrips = (props) => {    
+    const { tripList, reloadTripData, handleDeleteTrip, onLoadMore } = useTripData(props.showMyTrips)
+    const { isAuthenticated, user } = useAuth0()
     const [modalMap, setModalMap] = useState({
       modalVisible: false,
       tripLocations: null,
     })
-
     const [scrollToTripId, setScrollToTripId] = useState('')
     const lastItemRef = useRef(null)
 
-    const { getAccessTokenSilently, isAuthenticated, user } = useAuth0()
-    const displayLimit = 100
+    const onLoadMoreClicked = async () => {
+      const { noMoreRecords, scrollToTripId } = await onLoadMore()
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const res = (props.showMyTrips) ?
-          await tripService.getTrips(getAccessTokenSilently)
-          : await tripService.getPublicTrips(null)
-
-        setTripList( {
-          ...tripList,
-          trips: res,
-          loadingData: false,
-        })
-      }
-
-      fetchData()
-    }, [])
-
-    const reloadTripData = async () => {
-      setTripList( {
-        ...tripList,
-        loadingData: true,
-      })
-
-      const res = await tripService.getPublicTrips(null)
-
-      setTripList( {
-        trips: res,
-        loadingData: false,
-        noMoreRecords: false,
-      })
-    }
-
-    const onLoadMore = async () => {
-      setTripList({
-        ...tripList,
-        loadingData: true,
-      })
-      
-      const res = await tripService.getPublicTrips(tripList.trips[tripList.trips.length-1])
-
-      let updatedData = tripList.trips.concat(res)
-      let updateScrollPos = false
-      if (updatedData.length > displayLimit){
-        setScrollToTripId(tripList.trips[tripList.trips.length-1]._id)
-        updateScrollPos = true
-        
-        updatedData = updatedData.slice(updatedData.length - displayLimit)
-        updatedData[0] = { placeholder: true }
-      }
-      const noMoreRecords = res.length === 0 || res[res.length-1].noMoreRecords
-      
-      setTripList({
-        trips: updatedData,
-        loadingData: false,
-        noMoreRecords: noMoreRecords,
-      })
       if (noMoreRecords){
         message.info('No more older public trips to display')
       }
-
-      if (updateScrollPos && lastItemRef.current){
-        lastItemRef.current.scrollIntoView()
-      }
-    }
-
-    const handleDeleteTrip = async (tripId, tripTitle) => {
-      const res = await tripService.deleteTrip(tripId, tripTitle, getAccessTokenSilently)
-      
-      if (res){
-        setTripList({
-          ...tripList,
-          trips: tripList.trips.filter((trip) => { return trip._id !== tripId })
-        })
+      if (scrollToTripId !== -1){
+        setScrollToTripId(scrollToTripId)
+        if (lastItemRef.current){
+          lastItemRef.current.scrollIntoView()
+        }
       }
     }
     const handleLaunchMapModal = (tripTitle, tripLocations) => {
@@ -110,7 +41,7 @@ const DisplayTrips = (props) => {
       })
     }
 
-    const tripCountsPerSize = 10
+    const tripCountsPerPage = 10
     const showMyTrips = props.showMyTrips
     const handleEditTrip = props.editTrip
     let mapCenterLat = null
@@ -123,7 +54,7 @@ const DisplayTrips = (props) => {
 
     const loadMoreButton = (!tripList.loadingData && !tripList.noMoreRecords) ? 
                               (<div style={{ textAlign: 'center', marginTop: 12, }} >
-                                <Button onClick={onLoadMore}>Load more</Button>
+                                <Button onClick={onLoadMoreClicked}>Load more</Button>
                               </div>)
                               : null
     const tripCard = (item, itemRef) =>
@@ -147,85 +78,85 @@ const DisplayTrips = (props) => {
 
     return (
       <div className="displayTripsContainer">
-      <BackTop />
-        <Row
-          gutter={[0, 8]}
-          justify="start" >
-          <Col span={2}/>
-          <Col span={20}>
-          <Typography.Title>{greetingMsg(showMyTrips)}</Typography.Title>
-          </Col>
-          <Col span={2}/>
-        </Row>
+        <BackTop />
+          <Row
+            gutter={[0, 8]}
+            justify="start" >
+            <Col span={2} />
+            <Col span={20}>
+              <Typography.Title>{greetingMsg(showMyTrips)}</Typography.Title>
+            </Col>
+            <Col span={2} />
+          </Row>
 
-        <Row
-          gutter={[8, 16]}
-          justify="center" >
-          <Col span={4} />
-          <Col span={16}>
-            { !showMyTrips && tripList.trips.length > 0 && 
-            <List
-              itemLayout="vertical"
-              loading={tripList.loadingData}
-              loadMore={loadMoreButton}
-              dataSource={tripList.trips}
-              renderItem={(item) => (
-                <List.Item>
-                  {item._id === scrollToTripId && 
-                    tripCard(item, lastItemRef)
-                  }
-                  {item._id !== scrollToTripId && 
-                    tripCard(item, null)
-                  }
-                </List.Item>
-              )} />
-            }
-            { showMyTrips && tripList.trips.length > 0 && 
-            <List
-              itemLayout="vertical"
-              pagination={{
-                onChange: page=> {
-                  window.scrollTo({top:0, behavior: 'smooth'})
-                },
-                pageSize: tripCountsPerSize,
-              }}
-              dataSource={tripList.trips}
-              renderItem={(item) => (
-                <List.Item>
-                  {tripCard(item)}
-                </List.Item>
-              )} />
-            }
-            { tripList.loadingData &&
-            <Skeleton loading={true} active />
-            }
-            { tripList.trips.length === 0 && !tripList.loadingData &&
-            <Empty />
-            }
-          </Col>
-          <Col span={4} />
-        </Row>
-      <Modal
-        title='Trip locations'
-        visible={modalMap.modalVisible}
-        maskClosable={false}
-        bodyStyle={{height: '500px'}}
-        width='500px'
-        onCancel={handleModalOk}
-        footer={[
-          <Button
-              key="back"
-              type="primary"
-              onClick={handleModalOk} >
-              Close
-          </Button>
-        ]} >
-        <MyMapContainer
-            searchMode={false}
-            tripLocations={modalMap.tripLocations}
-            mapCenterLat={mapCenterLat}
-            mapCenterLng={mapCenterLng} />
-      </Modal>
+          <Row
+            gutter={[8, 16]}
+            justify="center" >
+            <Col span={4} />
+            <Col span={16}>
+              { !showMyTrips && tripList.trips.length > 0 && 
+              <List
+                itemLayout="vertical"
+                loading={tripList.loadingData}
+                loadMore={loadMoreButton}
+                dataSource={tripList.trips}
+                renderItem={(item) => (
+                  <List.Item>
+                    {item._id === scrollToTripId && 
+                      tripCard(item, lastItemRef)
+                    }
+                    {item._id !== scrollToTripId && 
+                      tripCard(item, null)
+                    }
+                  </List.Item>
+                )} />
+              }
+              { showMyTrips && tripList.trips.length > 0 && 
+              <List
+                itemLayout="vertical"
+                pagination={{
+                  onChange: page=> {
+                    window.scrollTo({top:0, behavior: 'smooth'})
+                  },
+                  pageSize: tripCountsPerPage,
+                }}
+                dataSource={tripList.trips}
+                renderItem={(item) => (
+                  <List.Item>
+                    {tripCard(item)}
+                  </List.Item>
+                )} />
+              }
+              { tripList.loadingData &&
+              <Skeleton loading={true} active />
+              }
+              { tripList.trips.length === 0 && !tripList.loadingData &&
+              <Empty />
+              }
+            </Col>
+            <Col span={4} />
+          </Row>
+        <Modal
+          title='Trip locations'
+          visible={modalMap.modalVisible}
+          maskClosable={false}
+          bodyStyle={{height: '500px'}}
+          width='500px'
+          onCancel={handleModalOk}
+          footer={[
+            <Button
+                key="back"
+                type="primary"
+                onClick={handleModalOk} >
+                Close
+            </Button>
+          ]} >
+          <MyMapContainer
+              searchMode={false}
+              tripLocations={modalMap.tripLocations}
+              mapCenterLat={mapCenterLat}
+              mapCenterLng={mapCenterLng} />
+        </Modal>
       </div>
       )
 }
