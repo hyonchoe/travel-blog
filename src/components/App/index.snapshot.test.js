7a9009a143331976e0b9bdfc88e438e34ba0d4fd
hyon choe <choe132@hotmail.com>
2020-07-17 23:08:21 -0500

    added snapshot testing for App

---
+++ src/components/App/index.snapshot.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './'
+
+describe('App', () => {
+    it('matches snapshot', () => {
+        const wrapper = shallow(
+            <App />
+        )
+        expect(wrapper).toMatchSnapshot()
+    })
+})
