204f2977853f9aeb360f8aed0fcd92acfe0a017f
hyon choe <choe132@hotmail.com>
2020-07-18 17:58:33 -0500

    updated snapshot test for NavBar

--- src/components/NavBar/index.snapshot.test.js
+++ src/components/NavBar/index.snapshot.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import * as hooks from '@auth0/auth0-react'
+import * as windowHooks from './useWindowDimensions'
 import NavBar from './'
 
 jest.mock('react-router-dom', () => ({
@@ -11,30 +12,72 @@ jest.mock('react-router-dom', () => ({
 }))
 
 describe('NavBar', () => {
-    it('matches snapshot authenticated', () => {
-        jest.spyOn(hooks, 'useAuth0').mockImplementation(() => ({
-            isAuthenticated: true,
-            user: { name: 'dummyname', email: 'dummyemail', sub: 'dummysub' },
-            logout: jest.fn(),
-            loginWithPopup: jest.fn()
-        }))
-        
-        const wrapper = shallow(
-            <NavBar />
-        )
-        expect(wrapper).toMatchSnapshot()
-    })
+    describe('for full menu', () =>{
+        beforeEach(() => {
+            jest.spyOn(windowHooks, 'default').mockImplementation(() => ({
+                windowWidth: 1000
+            }))
+        })
 
-    it('matches snapshot not authenticated', () => {
-        jest.spyOn(hooks, 'useAuth0').mockImplementation(() => ({
-            isAuthenticated: false,
-            user: null,
-            logout: jest.fn(),
-            loginWithPopup: jest.fn()
-        }))
-        const wrapper = shallow(
-            <NavBar />
-        )
-        expect(wrapper).toMatchSnapshot()
+        it('matches snapshot authenticated', () => {
+            jest.spyOn(hooks, 'useAuth0').mockImplementation(() => ({
+                isAuthenticated: true,
+                user: { name: 'dummyname', email: 'dummyemail', sub: 'dummysub' },
+                logout: jest.fn(),
+                loginWithPopup: jest.fn()
+            }))
+            const wrapper = shallow(
+                <NavBar />
+            )
+            expect(wrapper).toMatchSnapshot()
+        })
+    
+        it('matches snapshot not authenticated', () => {
+            jest.spyOn(hooks, 'useAuth0').mockImplementation(() => ({
+                isAuthenticated: false,
+                user: null,
+                logout: jest.fn(),
+                loginWithPopup: jest.fn()
+            }))
+            const wrapper = shallow(
+                <NavBar />
+            )
+            expect(wrapper).toMatchSnapshot()
+        })
+    })
+    
+    describe('for collapsed menu', () => {
+        beforeEach(() => {
+            jest.spyOn(windowHooks, 'default').mockImplementation(() => ({
+                windowWidth: 400
+            }))
+        })
+
+        it('matches snapshot authenticated', () => {
+            jest.spyOn(hooks, 'useAuth0').mockImplementation(() => ({
+                isAuthenticated: true,
+                user: { name: 'dummyname', email: 'dummyemail', sub: 'dummysub' },
+                logout: jest.fn(),
+                loginWithPopup: jest.fn()
+            }))
+            
+            const wrapper = shallow(
+                <NavBar />
+            )
+            expect(wrapper).toMatchSnapshot()
+        })
+    
+        it('matches snapshot not authenticated', () => {
+            jest.spyOn(hooks, 'useAuth0').mockImplementation(() => ({
+                isAuthenticated: false,
+                user: null,
+                logout: jest.fn(),
+                loginWithPopup: jest.fn()
+            }))
+            const wrapper = shallow(
+                <NavBar />
+            )
+            expect(wrapper).toMatchSnapshot()
+        })
     })
 })
