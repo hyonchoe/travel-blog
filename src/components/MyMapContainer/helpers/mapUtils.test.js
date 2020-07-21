08687416e7eb65a6e4f68b15df50d589362179a2
hyon choe <choe132@hotmail.com>
2020-07-21 14:37:23 -0500

    test update

--- src/components/MyMapContainer/helpers/mapUtils.test.js
+++ src/components/MyMapContainer/helpers/mapUtils.test.js
@@ -6,29 +6,42 @@
 
 import { getLocAddrInfo, ADDR_TYPES } from './mapUtils'
 
+const city = {
+    longName: 'city_longname',
+    shortName: 'city_shortname'
+}
+const state = {
+    longName: 'state_longname',
+    shortName: 'state_shortname'
+}
+const country = {
+    longName: 'country_longname',
+    shortName: 'country_shortname'
+}
+
 describe('Processing Google Places address data for city, state, and country', () => {
     it('returns valid found information', () => {
         const dummyAddrComponent = [
             {
-                long_name: 'state_longname',
-                short_name: 'state_shortname',
+                long_name: state.longName,
+                short_name: state.shortName,
                 types: [ADDR_TYPES.state]
             },
             {
-                long_name: 'city_longname',
-                short_name: 'city_shortname',
+                long_name: city.longName,
+                short_name: city.shortName,
                 types: [ADDR_TYPES.city]
             },
             {
-                long_name: 'country_longname',
-                short_name: 'country_shortname',
+                long_name: country.longName,
+                short_name: country.shortName,
                 types: [ADDR_TYPES.country]
             },
         ]
         const expected = { 
-            city: 'city_longname',
-            state: 'state_shortname',
-            country: 'country_longname'
+            city: city.longName,
+            state: state.shortName,
+            country: country.longName
          }
         expect(getLocAddrInfo(dummyAddrComponent)).toEqual(expected)
     })
@@ -36,18 +49,18 @@ describe('Processing Google Places address data for city, state, and country', (
     it('returns no valid found information', () => {
         const dummyAddrComponent = [
             {
-                long_name: 'state_longname',
-                short_name: 'state_shortname',
+                long_name: state.longName,
+                short_name: state.shortName,
                 types: ['someothertype']
             },
             {
-                long_name: 'city_longname',
-                short_name: 'city_shortname',
+                long_name: city.longName,
+                short_name: city.shortName,
                 types: ['someothertype']
             },
             {
-                long_name: 'country_longname',
-                short_name: 'country_shortname',
+                long_name: country.longName,
+                short_name: country.shortName,
                 types: ['someothertype']
             },
         ]
