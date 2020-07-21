3964afcf9e161040cc4b5ad54251cdb6859a6d3f
hyon choe <choe132@hotmail.com>
2020-07-21 14:28:21 -0500

    consolidate mock data for testing

--- src/components/PictureCarousel/index.snapshot.test.js
+++ src/components/PictureCarousel/index.snapshot.test.js
@@ -6,16 +6,13 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+import mockData from '../../testutils/mockData'
 import PictureCarousel from './'
 
 describe('PictureCarousel', () => {
     it('matches snapshot for image divs it hosts', () => {
-        const tripImages = [
-            { name: 'dummyname', S3Url: 'dummyurl' },
-            { name: 'dummyname2', S3Url: 'dummyurl2' },
-        ]
         const wrapper = shallow(
-            <PictureCarousel images={tripImages} />
+            <PictureCarousel images={mockData().images} />
         )
         expect(wrapper).toMatchSnapshot()
     })
